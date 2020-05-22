10cb35f0694f709e7b08c9447b2da7463e6c1d94
佐藤一成 <kazunarisato0309@gmail.com>
2020-05-23 00:19:57 +0900

    Initial Commit

---
+++ CRUD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CRUD
+//
+//  Created by 佐藤一成 on 2020/05/23.
+//  Copyright © 2020 s140. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
